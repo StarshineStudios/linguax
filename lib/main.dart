52b0c10d2cd5e00f752c9dc7e4c61ee59374173d
Saebyul Hong <studiosstarshine@gmail.com>
2023-08-13 23:06:20 -0400

    added a splash screen and a bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,18 @@
 import 'package:flutter/material.dart';
+import 'pages/first_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
       home: FirstPage(),
-    );
-  }
-}
-
-class FirstPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('App Title Test!'),
-      ),
-      body: Center(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Color(0xFFB4C3CF),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue,
-                  onPrimary: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) {
-                        return SecondPage();
-                      },
-                    ),
-                  );
-                },
-                child: Text('buttonName'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatefulWidget {
-  @override
-  _SecondPageState createState() => _SecondPageState();
-}
-
-class _SecondPageState extends State<SecondPage> {
-  int lions = 100;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('thing'),
-      ),
-      body: Center(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    lions *= -1;
-                  });
-                },
-                child: Image.asset('images/download.jpg'),
-              ),
-              Text(lions.toString()),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
