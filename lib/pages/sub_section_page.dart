3dcbac1244fa27587bfd9d30a51369906ef621ef
Saebyul Hong <104213415+StarshineStudios@users.noreply.github.com>
2023-08-21 22:38:31 -0400

    reorganized some files

---
+++ lib/pages/sub_section_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SubSectionPage extends StatelessWidget {
+  final String subSectionTitle;
+  final String buttonLabel;
+
+  const SubSectionPage(this.subSectionTitle, this.buttonLabel);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(buttonLabel),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Sub-section: $subSectionTitle'),
+            Text('Button: $buttonLabel'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Navigate back to the previous page
+              },
+              child: Text('Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
